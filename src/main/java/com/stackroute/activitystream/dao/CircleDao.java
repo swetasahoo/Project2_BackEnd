404940ccab1c84de25a13b72bfa98a4d3dfa0526
abbas886 <abbas886@yahoo.com>
2017-09-05 15:52:38 +0530

    Need to send only circle ids.  Not details.

--- src/main/java/com/stackroute/activitystream/dao/CircleDao.java
+++ src/main/java/com/stackroute/activitystream/dao/CircleDao.java
@@ -11,6 +11,8 @@ public interface CircleDao {
 	boolean addCircle(Circle circle);
 	boolean removeCircle(Circle circle);
 	List<Circle> getAllCircles();
+	//Need to send only circle ids.  Not details. 
+	//this should be getCirclesCreatedByMe.  This method is confusing.
 	List<Circle> getCircleByUser(String createdBy);
 	public boolean isCircleExist(String circleName);
 	
