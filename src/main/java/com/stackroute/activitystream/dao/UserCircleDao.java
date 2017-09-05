e905308454e14bea4de7600f2e3b029d7f778a86
abbas886 <abbas886@yahoo.com>
2017-09-05 15:57:07 +0530

    Change method signature

--- src/main/java/com/stackroute/activitystream/dao/UserCircleDao.java
+++ src/main/java/com/stackroute/activitystream/dao/UserCircleDao.java
@@ -8,8 +8,10 @@ public interface UserCircleDao {
 
 	boolean addUserToCircle(String emailId,String circleName);
 
+	//addUserToCircle and deleteUserFromCircle should have same parameters.
 	boolean deleteUserFromCircle(UserCircle userCircle);
 
+	//This method should return only list of email ids.  not List<UserCircle>
 	List<UserCircle> getUserByCircle(String circleName);
 
 	boolean isUserExistInCircle(String emailId,String circleName);
