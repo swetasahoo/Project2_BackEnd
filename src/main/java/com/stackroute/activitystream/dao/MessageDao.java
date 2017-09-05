30f783d9be4188b600753d54ed6c1cefc6a46bb1
abbas886 <abbas886@yahoo.com>
2017-09-05 15:55:11 +0530

    change the method signature.

--- src/main/java/com/stackroute/activitystream/dao/MessageDao.java
+++ src/main/java/com/stackroute/activitystream/dao/MessageDao.java
@@ -11,6 +11,10 @@ import com.stackroute.activitystream.model.Message;
 public interface MessageDao {
 
 boolean sendMessage(Message message);
+  //This method will return all the messages.  Need to return only the messages related to me.
+  //So need to pass email id.  One should not able to read other's messages.
 List<Message> getAllMessages();
 List<Message> getAllMessageByCircleName(String circleName);
+  
+  //Need to use pagination concept.
 }
