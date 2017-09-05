e197e1665fa322f0e4e2e2115f7aad2381b334fc
abbas886 <abbas886@yahoo.com>
2017-09-05 16:07:57 +0530

    two daos not required.

--- src/main/java/com/stackroute/activitystream/dao/MessageDao.java
+++ src/main/java/com/stackroute/activitystream/dao/MessageDao.java
@@ -6,7 +6,16 @@ import java.util.List;
 
 import com.stackroute.activitystream.model.Message;
 
+//1
+//Actually two daos are not required - MessageDao and UserMessageDao
+//  But two tables are required. ( message and user_message)
+//  At the time of sending the message, store the information in both the tables.
+//  At the time of reading/deleting use user_message table.
 
+//2
+//If you want to keep two separate daos
+//use MessageDao for sending the message (but store in both the tables.)
+//use UserMEssageDao for retreving and deleting the message.
 
 public interface MessageDao {
 
